0305903f1397ceb97ae94e85a36b44713dbcdc0a
H82M19\Student <Student>
2025-03-23 13:07:16 +0700

    Sample for layout

---
+++ src/ontap/layout/Sample.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Sample() {
+  return (
+    <div className=' flex gap-10 flex-wrap-reverse'>
+        <div className='bg-green-500 w-[30%] h-[200px] rounded-md'></div>
+        <div className='bg-red-500 w-[30%] h-[200px] rounded-md'></div>
+        <div className='bg-blue-500 w-[30%] h-[200px] rounded-md'></div>
+        <div className='bg-green-500 w-[30%] h-[200px] rounded-md'></div>
+        <div className='bg-red-500 w-[30%] h-[200px] rounded-md'></div>
+        <div className='bg-blue-500 w-[30%] h-[200px] rounded-md'></div>
+        <div className='bg-green-500 w-[30%] h-[200px] rounded-md'></div>
+        <div className='bg-red-500 w-[30%] h-[200px] rounded-md'></div>
+        <div className='bg-blue-500 w-[30%] h-[200px] rounded-md'></div>
+        <div className='bg-green-500 w-[30%] h-[200px] rounded-md'></div>
+        <div className='bg-red-500 w-[30%] h-[200px] rounded-md'></div>
+        <div className='bg-blue-500 w-[30%] h-[200px] rounded-md'></div>
+    </div>
+  )
+}
+
+export default Sample
